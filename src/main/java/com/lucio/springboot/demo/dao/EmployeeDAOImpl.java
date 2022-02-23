5d43bad0766f57b9c29489ca9337dfa0a6ba2806
luc-dev10 <luc.dev1008@gmail.com>
2022-02-22 20:39:04 -0500

    add jpa methods for crm

--- src/main/java/com/lucio/springboot/demo/dao/EmployeeDAOImpl.java
+++ src/main/java/com/lucio/springboot/demo/dao/EmployeeDAOImpl.java
@@ -1,13 +1,12 @@
 package com.lucio.springboot.demo.dao;
 
 import com.lucio.springboot.demo.entity.Employee;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
@@ -25,43 +24,36 @@ public class EmployeeDAOImpl implements EmployeeDAO {
     @Override
     @Transactional
     public List<Employee> getEmployees() {
-        // Session
-        Session session = entityManager.unwrap(Session.class);
+
         // query
-        Query<Employee> query = session.createQuery("from Employee", Employee.class);
+        Query query = entityManager.createQuery("from Employee");
         // execute query for list
-        return query.getResultList();
+        return (List<Employee>) query.getResultList();
     }
 
     @Override
     @Transactional
     public Employee getEmployee(int id) {
-        // session
-        Session session = entityManager.unwrap(Session.class);
-        return session.get(Employee.class, id);
+        return entityManager.find(Employee.class, id);
     }
 
     @Override
     @Transactional
     public void addEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-        session.save(employee);
+        entityManager.merge(employee);
     }
 
     @Override
     @Transactional
     public void deleteEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Employee employee = session.get(Employee.class, id);
+        Employee employee = entityManager.find(Employee.class, id);
         if (employee != null)
-            session.delete(employee);
+            entityManager.remove(employee);
     }
 
     @Override
     @Transactional
     public Employee modifyEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-        session.update(employee);
-        return employee;
+        return entityManager.merge(employee);
     }
 }
